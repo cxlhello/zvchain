b45c076bba3d1d153389bdcfbd87e7653e1c068d
chenxiaoli <346004279@qq.com>
2019-11-07 13:48:18 +0800

    bug fix

--- browser/mysql/storage_excute.go
+++ browser/mysql/storage_excute.go
@@ -746,7 +746,7 @@ func (storage *Storage) AddTokenContract(tran *models.Transaction, log *models.L
 			}
 
 			src := ""
-			storage.db.Model(models.Transaction{}).Select("source").Where("contract_address = ?", common2.StringToAddress(log.Address)).Row().Scan(&src)
+			storage.db.Model(models.Transaction{}).Select("source").Where("contract_address = ? ", common2.StringToAddress(tran.Target)).Row().Scan(&src)
 			tokenContract.Creator = src
 			tokenContract.TransferTimes = 1
 			storage.db.Create(&tokenContract)
